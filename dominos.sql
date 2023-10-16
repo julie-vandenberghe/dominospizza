9ed08b46ae087292257b6e929be04b5eed3628bf
Julie <120843391+julie-vandenberghe@users.noreply.github.com>
2023-10-16 16:49:41 +0200

    ADD examples of queries
    queries to select and to update

--- dominos.sql
+++ dominos.sql
@@ -18,13 +18,14 @@ CREATE TABLE IF NOT EXISTS pizzas (
     prix DECIMAL(4, 2)
 );
 
+
 -- Insertion des données dans la table pizzas
 INSERT INTO pizzas
 (nom, taille, prix)
 VALUES
-('4 fromages', 2, 8.90),
-('Margherita', 1, 4.90),
-('Reine', 3, 10.90);
+("4 fromages", 2, 8.90),
+("Margherita", 1, 4.90),
+("Reine", 3, 10.90);
 
 
 -- Création de la table ingredients
@@ -32,6 +33,19 @@ CREATE TABLE IF NOT EXISTS ingredients (
     id TINYINT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(20) NOT NULL
 );
+
+
+-- Insertion des données dans la table ingrédients
+INSERT INTO ingredients
+(nom)
+VALUES
+/* Si on met ("mozarella", "tomate", "jambon") à la place de ce qu'il y a ci-dessous, ne fonctionnera pas car sera considéré comme ligne et non comme colonne */
+("mozarella"),
+("tomate"),
+("jambon"),
+("champignon"),
+("gorgonzola");
+
 
 
 -- Création de la table pizzas_ingredients
@@ -44,6 +58,19 @@ CREATE TABLE IF NOT EXISTS pizzas_ingredients (
 );
 
 
+-- Insertion des données dans la table pizzas_ingrédients afin de "lier" les pizzas et les ingrédients
+INSERT INTO pizzas_ingredients
+(pizza_id, ingredient_id)
+VALUES
+(1, 1),
+(1, 5),
+(2, 2),
+(2, 1),
+(3, 2),
+(3, 3),
+(3, 4);
+
+
 -- Exemple de création d'une clé étrangère
 -- CREATE TABLE personne (
 --     id SMALLINT PRIMARY KEY AUTO_INCREMENT,
@@ -53,4 +80,32 @@ CREATE TABLE IF NOT EXISTS pizzas_ingredients (
 --     FOREIGN KEY id_voiture REFERENCES voiture(id)
 -- );
 
+
+-- Exemple de requête pour afficher le nom et le prix des pizzas pour deux personnes
+SELECT nom, prix 
+FROM pizzas
+WHERE taille = 2;
+
+-- Exemple de requête pour afficher les pizzas pour au moins deux personnes ayant un prix inférieur à 10€
+SELECT nom 
+FROM pizzas
+WHERE taille >= 2 AND prix < 10;
+
+-- Exemple de requête pour compter le nombre de pizzas à la carte 
+SELECT COUNT(*) AS nb_pizzas
+FROM pizzas;
+
+-- Exemple de requête pour maj (augmenter) le prix des pizzas pour 2 personnes
+UPDATE pizzas
+SET prix = prix*1.1
+WHERE taille = 2;
+
+
+-- Exemple de requête pour obtenir la liste des ingrédients pour chaque pizza
+SELECT *
+FROM ingredients
+INNER JOIN pizzas_ingredients
+ON ingredients.id = pizzas_ingredients.ingredient_id 
+INNER JOIN pizzas
+ON pizzas_ingredients.pizza_id = pizzas.id
 
